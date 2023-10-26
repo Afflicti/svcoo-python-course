df538d0bc9e17954270095222a950b9ddb77e7e4
Afflicti <kuba.kopcansky@gmail.com>
2023-10-26 16:26:19 +0200

    new lesson added

---
+++ lesson_06.py
@@ -0,0 +1,25 @@
+
+print("c1:")
+x = int(input())
+
+print("z:")
+zn = input()
+
+print("c2:")
+y = int(input())
+
+print(x, zn, y)
+
+if zn == "+":
+    print(x + y)
+
+if zn == "-":
+    print(x - y)
+
+if zn == "*":
+    print(x * y)
+
+    
+
+
+
