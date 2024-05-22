91a75c1df9a4c3b08dfc1f7fa177ef4f87f57235
Jakub Kopčanský <kuba.kopcansky@gmail.com>
2024-05-22 16:43:27 +0000

    lesson 27 added

---
+++ code_form_lessons/lesson_27/main.py
@@ -0,0 +1,20 @@
+
+human = {  #SHIFT + {}
+    "name":"Jakub",
+    "age":23,
+    "born":"Ostrava",
+    "cars":["Octacia", "Fabia"]
+}
+
+print(human)
+
+print(human.keys())
+print(human.values())
+print(human.items())
+
+print(human["name"])
+print(human["age"])
+
+print(human["cars"])
+print(human["cars"][0])
+
